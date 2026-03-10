e2b4f025a89f4b2f343483291d98febd55ef1dbe
ilfranco <ilaryafranco@gmail.com>
2026-03-10 19:43:52 -0400

    Updated Main Menu to look better

--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    void Start()
+    {
+        SceneManager.LoadScene("PhotoScene", LoadSceneMode.Additive);
+    }
+    
     public void Play()
     {
         SceneManager.LoadScene("SampleScene");
