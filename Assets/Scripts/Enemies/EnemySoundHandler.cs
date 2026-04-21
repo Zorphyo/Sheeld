53438a02ca93d8215383de46be561b3219862bf8
Real-Evan <ejw4431@gmail.com>
2026-04-21 15:31:11 -0400

    added Archer bow drawl and Heavy stomp sounds

--- Assets/Scripts/Enemies/EnemySoundHandler.cs
+++ Assets/Scripts/Enemies/EnemySoundHandler.cs
@@ -4,15 +4,20 @@ using UnityEngine.Audio;
 public class EnemySoundHandler : MonoBehaviour
 {
     public enum EnemyVoice { Male, Female }
+    public enum EnemyType  { Basic, Heavy, Archer, Medic }
 
-    [Header("Voice")]
+    [Header("Identity")]
     public EnemyVoice voice;
+    public EnemyType  type;
 
     [Header("Mixer")]
     public AudioMixerGroup mixerGroup;
 
     private AudioSource audioSource;
     private AudioClip attack, hit, death;
+    private AudioClip stomp;
+    private AudioClip arrowLoad;
+    private AudioClip medicHeal;
 
     void Awake()
     {
@@ -22,21 +27,45 @@ public class EnemySoundHandler : MonoBehaviour
 
         audioSource.outputAudioMixerGroup = mixerGroup;
 
-        if (voice == EnemyVoice.Male)
+        string gender = voice == EnemyVoice.Male ? "male" : "female";
+
+        // Every enemy type gets hit and death
+        hit   = Resources.Load<AudioClip>($"Sounds/{gender}/Hit");
+        death = Resources.Load<AudioClip>($"Sounds/{gender}/Death");
+
+        // Type-specific sounds
+        switch (type)
         {
-            attack = Resources.Load<AudioClip>("Sounds/male/Attack");
-            hit    = Resources.Load<AudioClip>("Sounds/male/Hit");
-            death  = Resources.Load<AudioClip>("Sounds/male/Death");
-        }
-        else
-        {
-            attack = Resources.Load<AudioClip>("Sounds/female/Attack");
-            hit    = Resources.Load<AudioClip>("Sounds/female/Hit");
-            death  = Resources.Load<AudioClip>("Sounds/female/Death");
+            case EnemyType.Basic:
+                attack = Resources.Load<AudioClip>($"Sounds/{gender}/Attack");
+                break;
+
+            case EnemyType.Heavy:
+                attack      = Resources.Load<AudioClip>($"Sounds/{gender}/Attack");
+                stomp = Resources.Load<AudioClip>("Sounds/heavy/Stomp");
+                break;
+
+            case EnemyType.Archer:
+                arrowLoad = Resources.Load<AudioClip>("Sounds/archer/ArrowLoad");
+                break;
+
+            case EnemyType.Medic:
+                medicHeal = Resources.Load<AudioClip>("Sounds/medic/Heal");
+                break;
         }
     }
 
-    public void PlayAttackSound() => audioSource.PlayOneShot(attack);
-    public void PlayHitSound()    => audioSource.PlayOneShot(hit);
-    public void PlayDeathSound()  => audioSource.PlayOneShot(death);
+    // Shared
+    public void PlayAttackSound()    => audioSource.PlayOneShot(attack);
+    public void PlayHitSound()       => audioSource.PlayOneShot(hit);
+    public void PlayDeathSound()     => audioSource.PlayOneShot(death);
+
+    // Heavy
+    public void PlayStompSound() => audioSource.PlayOneShot(stomp);
+
+    // Archer
+    public void PlayArrowLoadSound()  => audioSource.PlayOneShot(arrowLoad);
+
+    // Medic
+    public void PlayHealSound()        => audioSource.PlayOneShot(medicHeal);
 }
