e7a1e0dc67f8626bb8aaaaeb1ae0361fcc29515a
Sameer Syed <reachmeatsameer@gmail.com>
2026-04-29 02:23:24 -0400

    Start of scoremanager script

---
+++ Assets/Scripts/Core/ScoreManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance { get; private set; }
+
+    [Header("Score Values")]
+    public int score = 0;
+    public int damageDealt = 0;
+    public int multiHit = 0;
+
+    [Header("Multi-Hit Settings")]
+    public float multiHitTimeframe = 2f;
+
+    private float lastDamageTime = -999f;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        score = 0;
+        damageDealt = 0;
+        multiHit = 0;
+    }
+
+    public void ReportDamage(int damageAmount)
+    {
+        if (damageAmount <= 0)
+            return;
+
+        if (Time.time <= lastDamageTime + multiHitTimeframe)
+        {
+            multiHit++;
+        }
+
+        damageDealt += damageAmount;
+        lastDamageTime = Time.time;
+
+        score = Mathf.CeilToInt(damageDealt * (1.3f * (1 + multiHit)));
+
+        Debug.Log($"Score changed: {score} | DamageDealt: {damageDealt} | MultiHit: {multiHit}");
+    }
+}
