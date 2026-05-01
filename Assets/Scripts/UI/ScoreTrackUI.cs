8f984eb2555b41f4b78eb9d7eb3a883ffb0a517d
ilfranco <ilaryafranco@gmail.com>
2026-04-30 23:36:19 -0400

    Add HighScore Message

--- Assets/Scripts/UI/ScoreTrackUI.cs
+++ Assets/Scripts/UI/ScoreTrackUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -9,8 +10,9 @@ public class ScoreTrackUI : MonoBehaviour
     public int currentScore;
     public TextMeshProUGUI scoreCountText;
     public TextMeshProUGUI deathScoreText;
-
     public TextMeshProUGUI winScoreText;
+    public TextMeshProUGUI newHighScoreText;
+    public GameMode mode;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -28,12 +30,37 @@ public class ScoreTrackUI : MonoBehaviour
     public void deathPopupScore(int score)
     {
         deathScoreText.text = score.ToString();
+        mode = GameSession.CurrentMode;
+        CheckHighScore(score);
         return;
     }
 
     public void winPopupScore(int score)
     {
         winScoreText.text = score.ToString();
+        mode = GameSession.CurrentMode;
+        CheckHighScore(score);
+        return;
+    }
+
+    public void CheckHighScore(int currentScore)
+    {
+        List<ScoreEntry> scores = ScoreSystem.Instance.GetScores(mode);
+
+        if (scores == null || scores.Count == 0)
+        {
+            Debug.Log("No scores found yet — treating as new high score");
+            newHighScoreText.gameObject.SetActive(true);
+            return;
+        }
+
+        int highScore = scores.Max(x => x.score);
+
+        if (currentScore > highScore)
+        {
+            newHighScoreText.gameObject.SetActive(true);
+            Debug.Log("NEW HIGH SCORE!");
+        }
         return;
     }
 }
